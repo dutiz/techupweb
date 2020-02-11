2d2d793a0bc83af57383473e9137c04aa2697099
Duti <zaimidaut@gmail.com>
2020-02-11 17:37:04 +0100

    menu & photos

--- source/_partials/menu.blade.php
+++ source/_partials/menu.blade.php
@@ -1,10 +1,17 @@
 <div class="h-8 bg-blue-500"></div>
-<div class="h-24 bg-white flex items-center">
-    <div class="container">
-        <div class="row justify-between">
-            <div class="col-2">
-                <p class="font-bold text-gray-800 text-2xl">Web Starter</p>
-            </div>
+
+<div class="container">
+    <div class="row justify-between py-8 text-base text-black">
+        <div class="col-4">
+            <img src="/assets/images/logo.png" alt="">
+        </div>
+        <div class="col-8 hidden md:flex flex-row items-center justify-end">
+            <a class="transition font-medium mr-6 hover:text-purple-500" href="/">Home</a>
+            <a class="transition font-medium mr-6 hover:text-purple-500" href="/courses">All Courses</a>
+            <a class="transition font-medium mr-6 hover:text-purple-500" href="/instructors">Instructors</a>
+            <a class="transition font-medium mr-6 hover:text-purple-500" href="/blog">Blog</a>
+            <a class="transition font-medium hover:text-purple-500" href="/contact">Contact</a>
+            <button class="bg-blue-500 ml-8 py-2 px-6 rounded">Login/Register</button>
         </div>
     </div>
 </div>
