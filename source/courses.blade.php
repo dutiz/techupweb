8a84df7df5d2a823fa92af0b267b3761726f5ebe
Duti <zaimidaut@gmail.com>
2020-02-13 00:17:52 +0100

    fixing bugs

--- source/courses.blade.php
+++ source/courses.blade.php
@@ -5,14 +5,14 @@
 
 <div style="background:url('/assets/images/title_img.png'); ">
     <div class="container">
-        <div class="row items-center justify-between p-20">
+        <div class="row items-center justify-between py-20">
             <div class="col-4">
 
 
                 <h2 class="text-5xl"> All Courses </h2>
 
             </div>
-            <div class="col-3">
+            <div class="col-2">
                 <p class="text-sm"> Home > All Courses</p>
             </div>
         </div>
