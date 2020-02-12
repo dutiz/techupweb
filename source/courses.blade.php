c75d78598175b2c05c17e190d79d79062e7d4a60
Duti <zaimidaut@gmail.com>
2020-02-12 22:12:38 +0100

    All course page completed

---
+++ source/courses.blade.php
@@ -0,0 +1,24 @@
+@extends('_layouts.master')
+
+@section('body')
+
+
+<div style="background:url('/assets/images/title_img.png'); ">
+    <div class="container">
+        <div class="row items-center justify-between p-20">
+            <div class="col-4">
+
+
+                <h2 class="text-5xl"> All Courses </h2>
+
+            </div>
+            <div class="col-3">
+                <p class="text-sm"> Home > All Courses</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+@include('_partials.all_courses')
+
+@endsection
